1168a204305a225efeb44e45f99f853682009c3f
Lepticidio <miguel.andujar@live.u-tad.com>
2020-06-24 18:40:55 +0200

    Base Project for Practica1

---
+++ Practica1/game/Polygon.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "Polygon.h"
+Polygon::Polygon()
+{
+
+}
+void Polygon::DrawDebug()
+{
+    MOAIGfxDevice& gfxDevice = MOAIGfxDevice::Get();
+
+    gfxDevice.SetPenColor(0.0f, 0.5f, 1.0f, 0.5f);
+    MOAIDraw::DrawPolygon(m_tVertex);
+    gfxDevice.SetPenColor(0.0f, 0.5f, 1.0f, 0.05f);
+    MOAIDraw::DrawPolygonFilled(m_tVertex);
+}
